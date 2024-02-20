a45c2996b59db7e4483b1224c034d739ba77c9dc
unknown <ericryanbowser@gmail.com>
2024-02-20 00:57:13 -0700

    ss

---
+++ src/api/coinbase-api.js
@@ -0,0 +1,61 @@
+﻿const express = require('express');
+const WebSocket = require('ws');
+const https = require('https');
+
+const app = express();
+
+// Create a WebSocket server
+const ws = new WebSocket.Server({ noServer: true });
+
+// Function to handle WebSocket connection
+ws.on('connection', function connection(ws) {
+    console.log('WebSocket client connected');
+
+    // Subscribe to Coinbase ticker feed
+    const url = 'wss://ws-feed.pro.coinbase.com';
+    const subscribeMsg = JSON.stringify({
+        type: 'subscribe',
+        product_ids: ['BTC-USD'], // Change this to subscribe to different product
+        channels: ['ticker']
+    });
+
+    const connection = new WebSocket(url);
+
+    connection.on('open', async function open() {
+       await connection.send(subscribeMsg);
+    });
+
+    connection.on('message', async function incoming(data) {
+        await ws.send(data);
+    });
+
+    connection.on('close', async function close() {
+        console.log('WebSocket connection closed');
+    });
+
+    connection.on('error', async function error(err) {
+        console.error('WebSocket error:', err);
+    });
+
+    ws.on('close', async function close() {
+        console.log('WebSocket client disconnected');
+        await connection.close();
+    });
+});
+
+
+// Create an HTTP server
+const server = https.createServer(app);
+
+// Upgrade HTTP server to support WebSocket
+server.on('upgrade', async function upgrade(request, socket, head) {
+    await ws.handleUpgrade(request, socket, head, async function done(ws) {
+        await ws.emit('connection', ws, request);
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/components/TradeView.js
+++ src/components/TradeView.js
@@ -1,13 +1,21 @@
 ﻿import React, {useEffect, useState} from 'react';
 import {AdvancedChart} from "react-tradingview-embed";
-import {Container, Form, FormGroup, Navbar, NavbarBrand, NavItem, ToggleButton} from "react-bootstrap";
+import {
+    Container,
+    Form,
+    FormGroup,
+    Navbar,
+    NavbarBrand,
+    NavItem,
+    Table
+} from "react-bootstrap";
 import styled from "styled-components";
 
 const Grid = styled.div`
     padding: 15px;
-    background-color: white;
+    background-color: black;
     border: 5px solid black;
-    box-shadow: red 5px 5px 5px;
+    box-shadow: black 5px 5px 5px 1px;
     display: grid;
     grid-gap: 25px;
     grid-template-columns: 1fr;
@@ -28,11 +36,19 @@ const GridCol2 = styled.div`
 `;
 
 function TradeView(props = {}) {
+    // string 
     const [symbol, setSymbol] = useState("COINBASE:BTCUSD");
+    // Array
+    const [myLinks, setMyLinks] = useState([]);
+    // Boolean
+    const [spinner, setSpinner] = useState(false);
     const [darkModeChecked, setDarkModeChecked] = useState(false);
 
     useEffect(() => {
-    }, [symbol, darkModeChecked]);
+    }, [darkModeChecked, symbol]);
+
+    useEffect(() => {
+    }, [myLinks]);
 
     const selectSymbol = (event) => {
         if (event.target) {
@@ -47,58 +63,73 @@ function TradeView(props = {}) {
         }
     }
 
+    const pushLink = (event) => {
+        const link = event.target.value;
+        const arr = [];
+        if (event.target.value) {
+            arr.push(link);
+            setMyLinks(arr);
+        }
+    }
+
     const mode = darkModeChecked === true
         ? "dark"
         : "light";
 
     return (
-        <>
-            <Container>
-                <Navbar variant={'dark'}>
-                    <NavbarBrand>
-                        Brand
-                    </NavbarBrand>
-                    <NavItem>
-                        Test
-                    </NavItem>
-                </Navbar>
-                <Form>
-                    <FormGroup>
-                        <Form.Select
-                            aria-label="Floating label select"
-                            onChange={selectSymbol}
-                        >
-                            <option>Open this select menu</option>
-                            <option value="COINBASE:BTCUSD">BTC - USDC</option>
-                            <option value="COINBASE:ETHUSD">ETH - USDC</option>
-                            <option value="COINBASE:SOLUSD">SOL - USDC</option>
-                        </Form.Select>
-                        <Form.Group>
-                            <Form.Check
-                                onChange={setMode}
-                                checked={darkModeChecked}
-                                type={'checkbox'}
-                                label={'dark mode checkbox'}
-                                id={'dark-mode-checkbox'}
-                            />
-                        </Form.Group>
-                    </FormGroup>
-                </Form>
-            </Container>
-            <AdvancedChart widgetProps={
-                {
-                    allow_symbol_change: true,
-                    hide_side_toolbar: true,
-                    theme: mode,
-                    style: 1,
-                    symbol: symbol,
-                    range: 'ALL',
-                    show_popup_button: true,
-                    height: '700px',
-                    width: '1400px'
-                }
-            }/>
-        </>
+        <Grid>
+            <GridCol1>
+                <Container>
+                    <Navbar variant={'dark'} style={{boxShadow: 'white 5px 5px 5px 7px', margin: '25px'}}>
+                        <NavbarBrand>
+                            Brand
+                        </NavbarBrand>
+                        <NavItem>
+                            Test
+                        </NavItem>
+                    </Navbar>
+                    <Form>
+                        <FormGroup>
+                            <Form.Select
+                                aria-label="Floating label select"
+                                onChange={selectSymbol}
+                            >
+                                <option>Open this select menu</option>
+                                <option value="COINBASE:BTCUSD">BTC - USDC</option>
+                                <option value="COINBASE:ETHUSD">ETH - USDC</option>
+                                <option value="COINBASE:SOLUSD">SOL - USDC</option>
+                            </Form.Select>
+                            <Form.Group>
+                                <Form.Check
+                                    onChange={setMode}
+                                    checked={darkModeChecked}
+                                    type={'checkbox'}
+                                    label={'dark mode checkbox'}
+                                    id={'dark-mode-checkbox'}
+                                />
+                            </Form.Group>
+                        </FormGroup>
+                    </Form>
+                </Container>
+            </GridCol1>
+            <GridCol2>
+                {!spinner && (
+                    <AdvancedChart widgetProps={
+                        {
+                            allow_symbol_change: true,
+                            hide_side_toolbar: true,
+                            theme: mode,
+                            style: 1,
+                            symbol: symbol,
+                            range: 'ALL',
+                            show_popup_button: true,
+                            height: '700px',
+                            width: '1400px'
+                        }
+                    }/>
+                )}
+            </GridCol2>
+        </Grid>
     );
 }
 
